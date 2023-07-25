8c4fa9f350fca3cb3ce9cb13e4d76a6dee63db90
Nfon-Carl <nfoncarl48@gmail.com>
2023-07-25 15:50:35 +0100

    1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/home";
+import About from "./pages/about/about";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
 export default App;
