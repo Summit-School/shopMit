e3897d12e2279b66f696173b5fde6797b612dfc0
Nfon-Carl <nfoncarl48@gmail.com>
2023-07-26 15:28:00 +0100

    completed setting up routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Home from "./pages/home/home";
 import About from "./pages/about/about";
+import Login from "./pages/login/login";
+import Register from "./pages/register/register";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" component={Home} />
-        <Route path="/about" component={About} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
+    </Router>
   );
 }
 export default App;
---
+++ src/pages/register/register.jsx
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <>
+      <p>Registration Page</p>
+    </>
+  );
+};
+
+export default Register;
