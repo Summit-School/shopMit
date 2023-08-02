ba2b36f331bd7d2d21d5c8f135d594f2acec72f5
Nfon-Carl <nfoncarl48@gmail.com>
2023-08-02 09:35:03 +0100

    Worked on Registration & Login Pages

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,7 +1,46 @@
+import React, { useState } from "react";
+import "./login.css";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    //  logic to validate the user's credentials and perform the login action.
+    console.log("Logging in with email:", email, "and password:", password);
+  };
+
   return (
     <>
-      <p>Login Page</p>
+      <h1 className="shopTitle">ShopMit</h1>
+      <div className="login-container">
+        <form onSubmit={handleLogin} className="login-form">
+          <h2>Login</h2>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </>
   );
 };
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,8 +1,59 @@
+import React, { useState } from "react";
+import "./register.css";
+
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [registered, setRegistered] = useState(false);
+
+  const handleRegistration = (event) => {
+    event.preventDefault();
+    // For simplicity, we won't store the user data. We'll just show a message upon successful "registration".
+    setRegistered(true);
+  };
+
+  if (registered) {
+    return (
+      <div className="registrationBox">
+        <h1>Registration Successful!</h1>
+        <p>Your username: {username}</p>
+        <p>Your email: {email}</p>
+      </div>
+    );
+  }
+
   return (
-    <>
-      <p>Registration Page</p>
-    </>
+    <div className="registrationBox">
+      <h1>Registration Page</h1>
+      <form onSubmit={handleRegistration}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
   );
 };
 
